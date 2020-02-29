92a3a802ac7c5cab72f61c7a47b362663ccd4090
sarah <sarah@LesserArk>
2020-02-28 21:23:45 -0500

    Added authorization feature, some minor changes

--- src/main/java/com/protonmail/sarahszabo/mindwavemobiledataserver/core/Init.java
+++ src/main/java/com/protonmail/sarahszabo/mindwavemobiledataserver/core/Init.java
@@ -9,6 +9,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import org.json.JSONException;
 
 /**
  * The initialization class for the Neurosky Mindwave Mobile.
@@ -18,44 +19,58 @@ import java.nio.charset.Charset;
 public class Init {
 
     private static final String LOCAL_HOST = "127.0.0.1";
-    private static final int THINKGEAR_PORT = 13854;
+    private static final int THINKGEAR_PORT = 13854, MINDWAVE_SERVER_PORT = 45_000;
     private static final String TO_JSON_COMMAND = "{\"enableRawOutput\": false, \"format\": \"Json\"}\n";
+    private static final String AUTHORIZE_APP_COMMAND = "{\"appName\": \"MAX MSP Mindwave Server\", \"appKey\":"
+            + "\"55cf229b95b3fafa976b385af1b5670a817208d2\"}";
 
     /**
      * The main method where the server starts.
      *
      * @param args The command line arguments
      * @throws IOException If something happened
+     * @throws java.lang.InterruptedException If interrupted while sleeping
      */
     /*
-    TODO: Output to Ports
     TODO: Import Java Program into MAX
      */
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Connecting to host = " + LOCAL_HOST + ", port = " + THINKGEAR_PORT);
-        try (Socket clientSocket = new Socket(LOCAL_HOST, THINKGEAR_PORT)) {
+        try (var mindwaveSocket = new Socket(LOCAL_HOST, THINKGEAR_PORT)) {
             //Define variables
-            var input = clientSocket.getInputStream();
-            var reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
-            var output = clientSocket.getOutputStream();
+            var mindwaveInput = mindwaveSocket.getInputStream();
+            var mindwaveReader = new BufferedReader(new InputStreamReader(mindwaveInput, Charset.forName("UTF-8")));
+            var mindwaveOutput = mindwaveSocket.getOutputStream();
             //Write to output
-            System.out.println("Sending command: " + TO_JSON_COMMAND);
-            output.write(TO_JSON_COMMAND.getBytes());
-            output.flush();
+            System.out.println("Requesting Authorization: " + AUTHORIZE_APP_COMMAND);
+            mindwaveOutput.write(TO_JSON_COMMAND.getBytes());
+            mindwaveOutput.flush();
+            //Wait for authorization
+            System.out.println("Waiting for Authorization (5 Seconds)...");
+            Thread.sleep(5000);
+            if (mindwaveReader.ready()) {
+                System.out.println("Authorization: " + mindwaveReader.readLine());
+            }
             //Declare Output Stream to Max
-            var toMax = new DatagramSocket(new InetSocketAddress(LOCAL_HOST, 45_000));
-            var address = InetAddress.getByName("localhost");
-            //Start 60Hz Loop
-            while (true) {
-                if (reader.ready()) {
-                    String sourceJson = reader.readLine();
-                    var packet = new MindWavePacket(sourceJson);
-                    System.out.println("JSON TEXT RAW: \n\n" + sourceJson);
-                    var text = packet.toByteString().getBytes();
-                    var datagramPacket = new DatagramPacket(text, text.length, address, 45_000);
+            try (var maxmspOutputStream = new DatagramSocket(new InetSocketAddress(LOCAL_HOST, MINDWAVE_SERVER_PORT))) {
+                //Start 60Hz Loop
+                while (true) {
+                    if (mindwaveReader.ready()) {
+                        String sourceJson = mindwaveReader.readLine();
+                        System.out.println("JSON TEXT RAW: \n\n" + sourceJson);
+                        try {
+                            var packet = new MindWavePacket(sourceJson);
+                            System.out.println("\n\nMind Wave Packet: " + packet);
+                            var text = packet.toByteString().getBytes();
+                            var datagramPacket = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), MINDWAVE_SERVER_PORT);
+                            maxmspOutputStream.send(datagramPacket);
+                        } catch (JSONException je) {
+                            System.err.println("JSON Exception Caught, Continuing Loop");
+                        }
+                    }
+                    //16.6666666666 ms = 1/60 ms = 60Hz
+                    Thread.sleep(17);
                 }
-                //16.6666666666 ms = 1/60 ms = 60Hz
-                Thread.sleep(17);
             }
         }
     }
