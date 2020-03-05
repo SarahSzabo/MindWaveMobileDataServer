c54cdd3c6ad64b4e7fbdbd15220472d6a23c5749
sarah <sarah@LesserArk>
2020-03-05 11:08:23 -0500

    Made Mindwave Packet more compact

--- src/main/java/com/protonmail/sarahszabo/mindwavemobiledataserver/core/MindWavePacket.java
+++ src/main/java/com/protonmail/sarahszabo/mindwavemobiledataserver/core/MindWavePacket.java
@@ -6,9 +6,7 @@
 package com.protonmail.sarahszabo.mindwavemobiledataserver.core;
 
 import java.time.LocalTime;
-import java.time.ZonedDateTime;
 import java.util.logging.Logger;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -23,7 +21,9 @@ public class MindWavePacket {
 
     private final double attention, meditation, mentalEffort, familiarity, delta, theta, lowAlpha, highAlpha, lowBeta, highBeta, lowGamma, highGamma,
             blinkStrength, poorSignalLevel;
-    private final ZonedDateTime creationTime;
+    private final LocalTime creationTime;
+
+    private final int MINDWAVE_DEFAULT_NULL_VALUE = 0;
 
     public MindWavePacket(String originText) {
         /*
@@ -32,59 +32,55 @@ public class MindWavePacket {
         "eegPower":{"delta":1105014,"theta":211310,"lowAlpha":7730,"highAlpha":68568,"lowBeta":12949,"highBeta":47455,
         "lowGamma":55770,"highGamma":28247},"poorSignalLevel":0}
          */
-        this.creationTime = ZonedDateTime.now();
-        try {
-            JSONObject packet = new JSONObject(originText);
-            if (packet.has("eSense")) {
-                JSONObject eSense = packet.getJSONObject("eSense");
-                this.attention = eSense.getDouble("attention");
-                this.meditation = eSense.getDouble("meditation");
-            } else {
-                this.attention = -1;
-                this.meditation = -1;
-            }
-            if (packet.has("eegPower")) {
-                JSONObject eegPower = packet.getJSONObject("eegPower");
-                this.delta = eegPower.getDouble("delta");
-                this.theta = eegPower.getDouble("theta");
-                this.lowAlpha = eegPower.getDouble("lowAlpha");
-                this.highAlpha = eegPower.getDouble("highAlpha");
-                this.lowBeta = eegPower.getDouble("lowBeta");
-                this.highBeta = eegPower.getDouble("highBeta");
-                this.lowGamma = eegPower.getDouble("lowGamma");
-                this.highGamma = eegPower.getDouble("highGamma");
-            } else {
-                this.delta = -1;
-                this.theta = -1;
-                this.lowAlpha = -1;
-                this.highAlpha = -1;
-                this.lowBeta = -1;
-                this.highBeta = -1;
-                this.lowGamma = -1;
-                this.highGamma = -1;
-            }
-            if (packet.has("poorSignalLevel")) {
-                this.poorSignalLevel = packet.getInt("poorSignalLevel");
-            } else {
-                this.poorSignalLevel = -1;
-            }
-            if (packet.has("blinkStrength")) {
-                this.blinkStrength = packet.getInt("blinkStrength");
-            } else {
-                this.blinkStrength = -1;
-            }
-            if (packet.has("mentalEffort")) {
-                this.mentalEffort = packet.getInt("mentalEffort");
-            } else {
-                this.mentalEffort = -1;
-            }
-            if (packet.has("familiarity")) {
-                this.familiarity = packet.getInt("familiarity");
-            } else {
-                this.familiarity = -1;
-            }
-        } catch (JSONException js) {
-            throw new IllegalStateException("Unanticipated exception from JSON reading", js);
+        this.creationTime = LocalTime.now();
+        JSONObject packet = new JSONObject(originText);
+        if (packet.has("eSense")) {
+            JSONObject eSense = packet.getJSONObject("eSense");
+            this.attention = eSense.getDouble("attention");
+            this.meditation = eSense.getDouble("meditation");
+        } else {
+            this.attention = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.meditation = MINDWAVE_DEFAULT_NULL_VALUE;
+        }
+        if (packet.has("eegPower")) {
+            JSONObject eegPower = packet.getJSONObject("eegPower");
+            this.delta = eegPower.getDouble("delta");
+            this.theta = eegPower.getDouble("theta");
+            this.lowAlpha = eegPower.getDouble("lowAlpha");
+            this.highAlpha = eegPower.getDouble("highAlpha");
+            this.lowBeta = eegPower.getDouble("lowBeta");
+            this.highBeta = eegPower.getDouble("highBeta");
+            this.lowGamma = eegPower.getDouble("lowGamma");
+            this.highGamma = eegPower.getDouble("highGamma");
+        } else {
+            this.delta = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.theta = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.lowAlpha = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.highAlpha = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.lowBeta = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.highBeta = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.lowGamma = MINDWAVE_DEFAULT_NULL_VALUE;
+            this.highGamma = MINDWAVE_DEFAULT_NULL_VALUE;
+        }
+        if (packet.has("poorSignalLevel")) {
+            this.poorSignalLevel = packet.getInt("poorSignalLevel");
+        } else {
+            this.poorSignalLevel = MINDWAVE_DEFAULT_NULL_VALUE;
+        }
+        if (packet.has("blinkStrength")) {
+            this.blinkStrength = packet.getInt("blinkStrength");
+        } else {
+            this.blinkStrength = MINDWAVE_DEFAULT_NULL_VALUE;
+        }
+        if (packet.has("mentalEffort")) {
+            this.mentalEffort = packet.getInt("mentalEffort");
+        } else {
+            this.mentalEffort = MINDWAVE_DEFAULT_NULL_VALUE;
+        }
+        if (packet.has("familiarity")) {
+            this.familiarity = packet.getInt("familiarity");
+        } else {
+            this.familiarity = MINDWAVE_DEFAULT_NULL_VALUE;
         }
     }
 
@@ -104,7 +100,7 @@ public class MindWavePacket {
 
     @Override
     public String toString() {
-        return "Mind Wave Mobile Packet: " + LocalTime.now() + "\n\neSense:\n\nAttention: " + attention + "\nMeditation: " + meditation
+        return "Mind Wave Mobile Packet: " + this.creationTime + "\n\neSense:\n\nAttention: " + attention + "\nMeditation: " + meditation
                 + "\nMental Effort: " + this.mentalEffort + "\n Familiarity: " + this.familiarity
                 + "\n\nEEG Power:\n\nDelta: " + delta + "\nTheta: " + theta + "\nLow Alpha: " + lowAlpha + "\nHigh Alpha: " + highAlpha
                 + "\nLow Beta: " + lowBeta + "\nHigh Beta: " + highBeta + "\nLow Gamma: " + lowGamma + "\nHigh Gamma: " + highGamma
